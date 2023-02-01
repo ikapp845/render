3e3176e997df213a2fd1f2d178c57e953032c78f
ikapp845 <ikapp845@gmail.com>
2023-02-01 13:21:09 +0530

    s

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!63wen65hg68=*%3i-x4^%-4yzdj7adjiil8=n%h-3c3lzwa0&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
