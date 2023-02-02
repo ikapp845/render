0301ee628e796391fe3e9020b80514b89e2810ae
ikapp845 <ikapp845@gmail.com>
2023-02-02 21:35:47 +0530

    sd

--- group/urls.py
+++ group/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
   path("group_question/<str:group>/<str:username>/",views.group_question,name = "Group Question"),
   path("group_members/<str:group>/",views.group_members,name = "Group Members"),
   path("leave/",views.leave,name = "Leave Group"),
-  path("user_groups/<str:username>/",views.user,name = "UserGroups")
+  path("user_groups/<str:username>/",views.user_groups,name = "UserGroups")
 ]
