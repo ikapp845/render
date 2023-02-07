38a7ca97d241bfacb36efba4306cdc4065e87e4d
ikapp845 <ikapp845@gmail.com>
2023-02-07 20:52:43 +0530

    asdg

--- likes/serializer.py
+++ likes/serializer.py
@@ -8,6 +8,8 @@ class LikeSerializer(serializers.ModelSerializer):
   question = serializers.SerializerMethodField("get_question")
   from_username = serializers.SerializerMethodField("get_fromuser")
   from_gender = serializers.SerializerMethodField("get_fromgender")
+  question_likes = serializers.SerializerMethodField("get_question_likes")
+  total_question_like = serializers.SerializerMethodField("get_total_question_like")
 
   class Meta:
     model = Like
@@ -20,7 +22,6 @@ class LikeSerializer(serializers.ModelSerializer):
   def get_fromuser(self,like):
     user_to = like.user_to
     user_from = like.user_from
-    print(user_from.paid)
     if user_to.paid == "True":
       if user_from.paid == "True":
         return "From user paid"
@@ -34,5 +35,15 @@ class LikeSerializer(serializers.ModelSerializer):
     user_from = like.user_from
     return user_from.gender
       
+  def get_question_likes(self,like):
+    user_to = like.user_to
+    question = like.question
+    group = like.group
+    likes  = Like.objects.filter(question = question,group = group,user_to = user_to)
+    return len(likes)
 
-
+  def get_total_question_like(self,like):
+    question = like.question
+    group = like.group
+    likes = Like.objects.filter(question = question,group = group)
+    return len(likes)
