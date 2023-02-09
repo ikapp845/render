c8be4d334bd8fcc431d6a1e4ea49bf16c8a421a5
ikapp845 <ikapp845@gmail.com>
2023-02-09 20:55:39 +0530

    cawkn

--- likes/views.py
+++ likes/views.py
@@ -12,11 +12,12 @@ from group.models import Members,Group
 # {"username1":"","username2":"","question":""}
 @api_view(["POST"])
 def like(request):
+  print((request.data))
   req = request.data
   user1 = Profile.objects.get(name = req["username1"])
   user2 = Profile.objects.get(name = req["username2"])
   question = Question.objects.get(id = req["question"])
-  group = Group.objects.get(name = req["group"])
+  group = Group.objects.get(id = req["group"])
   like = Like.objects.create(user_from = user1,user_to =user2,question = question,group = group )
   like.save()
   return Response("Liked")
